e4a54a3d9f85a5b9f2bb3c054aca69279b9d4d0e
mdfarhanahmadkhan7786@gmail.com <affan@Affans-MacBook-Air.local>
2026-03-18 14:14:20 +0530

    learned passing parameter by address and reference

---
+++ Functions/func_pass_by_address.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void change(int* x, int* y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main(){
+    int a = 10;
+    int b = 20;
+    cout<<a<<" "<<b<<endl;
+    change(&a, &b);
+    cout<<a<<" "<<b<<endl;
+    return 0;
+}
