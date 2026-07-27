8e62a4ca6650ad5f04744e1bc8f02178f6d805b0
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-07-27 09:20:00 +0530

    wrote recursive algorithm for power of 2

--- Leet/main.cpp
+++ Leet/main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 using namespace std;
 
+// bool isPower(int n){
+//     if(n <= 0){
+//         return false;
+//     }
+//     if(n == 1){
+//         return true; 
+//     }
+//     while(n > 1){
+//             if(n%2 != 0){
+//                 return false;
+//             }
+//             n = n/2;
+//     }
+//     return true;
+// }
+
 bool isPower(int n){
-    if(n <= 0){
-        return false;
-    }
     if(n == 1){
         return true;
     }
-    while(n > 1){
-            if(n%2 != 0){
-                return false;
-            }
-            n = n/2;
+    if(n <= 0 || n % 2 != 0){
+        return false;
     }
-    return true;
+    return isPower(n/2);
 }
 
 int main(){
