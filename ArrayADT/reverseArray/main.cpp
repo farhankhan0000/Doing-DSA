d1fcf7de15e34f6f3e3d055ebcdba9fec01d6ea8
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-08-16 12:09:56 +0530

    another method of reversing an array

--- ArrayADT/reverseArray/main.cpp
+++ ArrayADT/reverseArray/main.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 using namespace std;
 
-void reverseArray(struct Array *arr, struct Array * Arr){
-    for (int i = 0,j = arr->length -1; i < j; i++, j--)
-    {
-        Arr->A[i] = arr->A[j];
-    }
+// void reverseArray(struct Array *arr, struct Array * Arr){
+//     for (int i = 0,j = arr->length -1; i < arr->length; i++, j--)
+//     {
+//         Arr->A[i] = arr->A[j];
+//     }
     
-    for (int i = 0; i < arr->length; i++)
+//     for (int i = 0; i < arr->length; i++)
+//     {
+//         arr->A[i] = Arr->A[i];
+//     }
+    
+// }
+
+// void reverseArray(struct Array *arr){
+//     for (int i = 0,j=arr->length-1; i < j; i++,j--)
+//     {
+//         int temp = arr->A[i];
+//         arr->A[i] = arr->A[j];
+//         arr->A[j] = temp;
+//     }
+    
+// }
+
+void reverseArray(struct Array *arr){
+    for (int i = 0; i < (arr->length)/2; i++)
     {
-        arr->A[i] = Arr->A[i];
+        int temp = arr->A[i];
+        arr->A[i] = arr->A[arr->length-i-1];
+        arr->A[arr->length-i-1] = temp;
     }
     
 }
