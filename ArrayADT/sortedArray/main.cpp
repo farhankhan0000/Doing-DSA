0d594c87e51142709baa03d4705700941a2394e3
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-08-27 13:49:25 +0530

    insert element in sorted array and check if the array is sorted or not

---
+++ ArrayADT/sortedArray/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+void Display(struct Array arr){
+    int i;
+    cout<<"\nElements are\n";
+    for (i = 0; i < arr.length; i++)
+    {
+        cout<<arr.A[i];
+    }
+}
+
+void insertElementInSorted(struct Array *arr, int x){
+    int j = arr->length - 1;
+    if(arr->length == arr->size){
+        return;
+    }
+    while(arr->A[j] > x){
+        arr->A[j++] = arr->A[j];
+        j--;
+    }
+    arr->A[j+1] = x;
+    arr->length ++;
+}
+
+int isSorted(struct Array arr){
+    int i;
+    for (i = 0; i < arr.length - 1; i++)
+    {
+        if(arr.A[i] > arr.A[i+1]){
+            return 0;
+        }
+        return 1;
+    }
+    
+}
+
+int main(){
+    struct Array arr1 = {{2,3,9,16,18,21,28,32,35},10,9};
+    cout<< isSorted(arr1)<<endl;
+    Display(arr1);
+    return 0;
+}
+
